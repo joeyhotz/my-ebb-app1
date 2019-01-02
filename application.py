9ca9533d89580be275b8904ef9e99e06e433c009
Joey Hotz <joeyhotz1@gmail.com>
2019-01-02 11:45:25 +1100

    Adds author to homepage

--- application.py
+++ application.py
@@ -112,6 +112,7 @@ welcome = """
   <div class="textColumn">
     <h1>Congratulations</h1>
     <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
+    <p>By Joey Hotz ©</p>
   </div>
   
   <div class="linksColumn"> 
